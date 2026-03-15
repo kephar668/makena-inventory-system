9e2e460ef5b9b990500239364e5f1362a529b56f
kephar668 <calebmuthomi26@gmail.com>
2026-03-16 00:15:56 +0300

    Create config.py for database and application configuration settings

---
+++ config.py
@@ -0,0 +1,8 @@
+# Configuration settings for the application
+
+class Config:
+    DATABASE_URI = 'sqlite:///makena_inventory.db'  # Example database URI
+    DEBUG = True  # Set to False in production
+    SECRET_KEY = 'your_secret_key_here'  # Replace with your secret key
+
+# Additional configuration settings can be added here
